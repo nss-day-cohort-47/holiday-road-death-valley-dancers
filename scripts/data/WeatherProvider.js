a3e81160a3a076182953662b319325da90f47282
abbeyroyse13 <heatherruan126@gmail.com>
2021-03-16 11:36:06 -0500

    fixed weather fetch

--- scripts/data/WeatherProvider.js
+++ scripts/data/WeatherProvider.js
@@ -1,10 +1,10 @@
-//import { settings } from "../Settings.js"
+import { settings } from "../Settings.js"
 
 let weatherForecast = [];
 
 export const getWeather = () => {
 
-    return fetch(`http: //api.openweathermap.org/data/2.5/forecast?id=524901&appid=e78bff76cd169fd6088b48c66c3fd622`)
+    return fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${settings.weatherKey}`)
         .then(response => response.json())
         .then(parsedResponse => {
             weatherForecast = parsedResponse;
