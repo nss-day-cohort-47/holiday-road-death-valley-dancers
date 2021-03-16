022cb6471caf6eabfe4dad6aeb7ac2444ac91fa4
Bryson <brysonmg@gmail.com>
2021-03-16 11:17:33 -0500

    ParkProvider gets data from the api

--- scripts/data/ParkProvider.js
+++ scripts/data/ParkProvider.js
@@ -1,1 +1,16 @@
-import { settings } from "../Settings.js"
+import {settings} from '../Settings.js'
+
+let allParks = [];
+
+export const getParks = () => {
+    return fetch(`https://developer.nps.gov/api/v1/parks?api_key=${settings.npsKey}`)
+    .then(response => response.json)
+    .then(parsedResponse => {
+        allParks = parsedResponse;
+        return parsedResponse;
+    })
+}
+
+export const useAllParks = () => {
+    return [...allParks]
+}
