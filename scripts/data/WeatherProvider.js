efecbfbdb348e5b1106a2ed0be0b99fffaf44c0b
abbeyroyse13 <heatherruan126@gmail.com>
2021-03-17 15:14:25 -0500

    committing weather stuff

--- scripts/data/WeatherProvider.js
+++ scripts/data/WeatherProvider.js
@@ -4,12 +4,21 @@ let weatherForecast = [];
 
 export const getWeather = () => {
 
-    return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Nashville,TN,840&units=imperial&appid=${settings.weatherKey}`)
+    return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Hodgenville,KY,840&units=imperial&appid=${settings.weatherKey}`)
         .then(response => response.json())
         .then(parsedResponse => {
             console.log(parsedResponse.list)
-            weatherForecast = parsedResponse;
-            return parsedResponse;
+
+            weatherForecast = parsedResponse.list;
+            let filteredWeather = [];
+            for (let i = 0; i < weatherForecast.length; i++) {
+                if (i === 0 || i === 7 || i === 15 || i === 23 || i === 31) {
+                    filteredWeather.push(weatherForecast[i])
+                }
+            }
+            console.log(filteredWeather);
+            weatherForecast = filteredWeather;
+            return filteredWeather;
         })
 }
 
