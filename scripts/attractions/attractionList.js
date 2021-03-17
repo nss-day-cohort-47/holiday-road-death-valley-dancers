4d55ff45b7ba677946c1bfb649aa9f1d31bc9e6c
abbeyroyse13 <heatherruan126@gmail.com>
2021-03-17 11:24:25 -0500

    uhh

--- scripts/attractions/attractionList.js
+++ scripts/attractions/attractionList.js
@@ -1,10 +1,10 @@
-import {attractionObj} from "./attractionObj.js";
+import { attractionObj } from "./attractionObj.js";
 
 //Iterating over data in HTML format and storing in a variable
 export const attractionList = (list) => {
     let attractionHTML = "";
     for (const obj of list) {
-        attractionHTML = attractionObj(obj)
+        attractionHTML += attractionObj(obj)
     }
     return attractionHTML;
 }
